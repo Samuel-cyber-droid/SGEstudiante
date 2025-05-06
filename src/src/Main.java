d7d18de9d1ccdb40d18a8347b51564fce0198fd3
Samuel-cyber-droid <samy21jul@gmail.com>
2025-05-05 20:38:24 -0600

    Creacion de Proyecto

---
+++ src/src/Main.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.lang.management.*;
+
+class Estudiante {
+    private String idEstudiante;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String carrera;
+
+    public Estudiante(String idEstudiante, String nombre, String apellido, int edad, String carrera){
+        this.idEstudiante = idEstudiante;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.carrera = carrera;
+    }
+
+    // Creacion de getters y setters
+
+    public String getIdEstudiante() {
+        return idEstudiante;
+    }
+    public void setIdEstudiante(String idEstudiante) {
+        this.idEstudiante = idEstudiante;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getCarrera() {
+        return carrera;
+    }
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + idEstudiante + ", Nombre: " + nombre + " " + apellido + ", Edad: " + edad + ", Carrera: " + carrera;
+    }
+}
+
+class SistemaGestionEstudiantes {
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
